3ad3eed484e7ed9099f1639346459682c147aa90
narendrasoni1989 <narendrasoni2@gmail.com>
2016-08-02 11:37:44 +0800

    Tool improved

--- tool.sh
+++ tool.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-pid_file=/Users/narendra/Documents/workspace/shellscript/jvmtool/pid
+pid_file=/Users/narendra/Documents/workspace/SourceTreeRepositories/java-heap-tool/pid
 
 pid=$(head -n 1 $pid_file)
 
 echo "$pid"
 
-buffer=1024.0 #1mb
-#buffer=24.0 #1mb
+#buffer=1024.0 #1mb
+buffer=800.0 #KB
 OC=`jstat -gc $pid | awk '{print $7}' | tail -1` #returns memory size in KB
 OU=`jstat -gc $pid | awk '{print $8}' | tail -1` #returns memory size in KB
+OGCMX=`jstat -gccapacity $pid | awk '{print $8}' | tail -1` #returns memory size in KB
 
 #http://docs.oracle.com/javase/1.5.0/docs/tooldocs/share/jstat.html#gc_option documentation on jstat
 
-echo "$OC"
-echo "$OU"
+echo "OC: $OC"
+echo "OU: $OU"
+echo "OGCMX: $OGCMX"
 
-result=`awk "BEGIN { print (($OC-$buffer) > $OU) }"`
-
+#result=`awk "BEGIN { print (($OC-$buffer) > $OU) }"`
+result=`awk "BEGIN { print (($OGCMX-$OU) > $buffer) }"`
 if [ "$result" = 0 ]; then
     echo "Killing program because JVM Memory leak is detected."
     kill -9 $pid
@@ -25,4 +27,4 @@ elif [ "$result" = 1 ]; then
 	echo "Program running fine."
 fi
 
-echo "$result"
+#echo "$result"
